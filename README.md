5864d86f8f79b0c4d48faabed838ffd07e713ab7
Aleksei_Valenkov <aleksejvalenkov@yandex.ru>
2024-06-02 15:59:30 +0300

    add install commrnt

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NN_path_planning
+
+## Install libs
+``` bash
+pip3 install -r requirements.txt 
+```
--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 from pathlib import Path
 from setuptools import setup, find_packages
 
-description = ['pattern_based_calibration']
+description = ['NN_path_planning']
 
 # Setting file paths
 root = Path(__file__).parent
 readme = root / 'README.md'
 reqs_main = root / 'requirements.txt'
-reqs_calibration_tools = root / 'packages/calibration_tools/requirements.txt'
-reqs_realsense_tools = root / 'packages/realsense_tools/requirements.txt'
-reqs_zed_tools = root / 'packages/zed_tools/requirements.txt'
-reqs_azure_tools = root / 'packages/azure_tools/requirements.txt'
 
-reqs = [reqs_main,reqs_calibration_tools, reqs_realsense_tools, reqs_zed_tools, reqs_azure_tools]
+
+reqs = [reqs_main]
 
 with open(readme, 'r', encoding='utf-8') as f:
     readme = f.read()
@@ -24,16 +21,16 @@ for req in reqs:
 
 
 setup(
-    name='pattern_based_calibration',
+    name='NN_path_planning',
     version='0.0.1',
     packages=find_packages(),
     python_requires='>=3.8',
     install_requires=dependencies,
-    author='Aleksei_Valenkov',
+    author='aleksejvalenkov',
     description=description,
     long_description=readme,
     long_description_content_type="text/markdown",
-    url='https://git.sberrobots.ru/vedu/doedu/-/tree/calib_3d_sensors/calibration/pattern_based_calibration?ref_type=heads',
+    url='https://github.com/aleksejvalenkov/NN_path_planning',
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
